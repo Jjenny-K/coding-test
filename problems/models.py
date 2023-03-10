d4a4f5da80bd864b4a009a324152d4ac3df40e95
Jjenny-K <kkjjh0129@gmail.com>
2023-03-10 19:41:58 +0900

    Feat: Question, Answer modeling

--- problems/models.py
+++ problems/models.py
@@ -1,3 +1,37 @@
 from django.db import models
 
-# Create your models here.
+from utils.timestamp import TimestampZone
+
+
+class Question(TimestampZone):
+    user = models.ForeignKey('accounts.User', verbose_name='작성자', on_delete=models.CASCADE)
+    title = models.CharField(verbose_name='문제명', max_length=255)
+    description = models.TextField(verbose_name='문제설명')
+    constraint = models.TextField(verbose_name='제한사항', blank=True, default='')
+    category = models.CharField(verbose_name='유형', max_length=31)
+
+    class DifficultyType(models.TextChoices):
+        EASY = 'EASY'
+        NORMAL = 'NORMAL'
+        HARD = 'HARD'
+
+    difficulty = models.CharField(verbose_name='난이도', max_length=15, choices=DifficultyType.choices)
+
+    class Meta:
+        db_table = 'question'
+
+    def __str__(self):
+        return f'question {self.category}-{self.title}'
+
+
+class Answer(TimestampZone):
+    user = models.ForeignKey('accounts.User', verbose_name='작성자', on_delete=models.CASCADE)
+    question = models.ForeignKey('problems.Question', verbose_name='문제', on_delete=models.CASCADE)
+    answer = models.CharField(verbose_name='답안', max_length=255)
+    description = models.TextField(verbose_name='해설')
+
+    class Meta:
+        db_table = 'answer'
+
+    def __str__(self):
+        return f'{self.question} answer'
